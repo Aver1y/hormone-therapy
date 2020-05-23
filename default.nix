8cc3475bd24c7b5eb27522e651a4f516df8ce1f4
Lila <lila91142@gmail.com>
2020-05-23 02:05:59 +0200

    Update pandoc-cite2url-hs

--- default.nix
+++ default.nix
@@ -10,8 +10,8 @@ developPackage {
     pandoc-url2cite-hs = fetchFromGitHub {
       owner = "Aver1y";
       repo = "pandoc-url2cite-hs";
-      rev = "d6fac9756f0954c4b9911113f9639dcbba5d8a0f";
-      sha256 = "0ksgpghsy064bn7rajlxk5a8a1b4shd20wp2l8pyifx04za9j1z6";
+      rev = "41e685d1f1788d3c152853bc72262ccbf02b5a07";
+      sha256 = "1bx9a3fb0xcnjs0fl9k43qak61ag2kmdc6d2056ia421c1r0dq2f";
     };
     hakyll = fetchFromGitHub {
       owner = "Aver1y";
